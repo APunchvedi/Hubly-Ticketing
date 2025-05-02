abdae573f4f8af4ca0da8fc3b997aaa1c2123de2
APunchvedi <punchvedi.akash96@gmail.com>
2025-05-02 16:50:41 +0530

    Cors check

--- backend/index.js
+++ backend/index.js
@@ -17,31 +17,50 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://hubly-ticketing-frontend.onrender.com",
+// ];
+
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+//   credentials: true,
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
+// app.options(
+//   "*",
+//   cors({
+//     origin: allowedOrigins,
+//     credentials: true,
+//   })
+// );
+
 const allowedOrigins = [
   "http://localhost:5173",
   "https://hubly-ticketing-frontend.onrender.com",
 ];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-app.options(
-  "*",
+app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 
 app.use(express.urlencoded({extended:true}))
 
