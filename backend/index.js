df9b020e25a8598dbefd1d44faf7f415ea0a59da
APunchvedi <punchvedi.akash96@gmail.com>
2025-05-02 12:09:11 +0530

    2nd commit

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,10 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://hubly-ticketing-frontend.onrender.com",
+];
 
 app.use(cors({
   origin: (origin, callback) => {
